e01dca68c60bf6dda839f92eb5b75a7b03cb3ce4
gemy <mohamed.gamal.mg136@gmail.com>
2022-10-20 13:45:37 +0200

    update notfound page

--- src/components/notfound/index.jsx
+++ src/components/notfound/index.jsx
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
-import './notFound.css'
+import "./notFound.css";
 
 const NotFound = () => {
   return (
     <section class="d-flex flex-column align-items-center justify-content-center">
-      <h1 class="text-light notFound">
+      <h1 class=" text-dark notFound">
         4<span>0</span>4
       </h1>
-      <p class="text-light fs-1">Oops! Something is wrong.</p>
+      <p class=" fs-1">Oops! Something is wrong.</p>
       <Link class="shadow fs-4 fw-bold goHome" to="/">
-        Go Home
+         Home Page
       </Link>
     </section>
   );
